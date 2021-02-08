3a17fe7e6430e4c2715596add4a05d52b93fe5a8
Emil InnerW <emil4uk273@gmail.com>
2021-02-08 20:02:17 +0400

    first

---
+++ SushiBarBusinessLogic/BusinessLogics/SushiLogic.cs
@@ -0,0 +1,63 @@
+﻿using SushiBarBusinessLogic.BindingModels;
+using SushiBarBusinessLogic.Interfaces;
+using SushiBarBusinessLogic.ViewModels;
+using System;
+using System.Collections.Generic;
+
+
+namespace SushiBarBusinessLogic.BusinessLogics
+{
+    public class SushiLogic
+    {
+        private readonly ISushiStorage _productStorage;
+        public SushiLogic(ISushiStorage productStorage)
+        {
+            _productStorage = productStorage;
+        }
+        public List<SushiViewModel> Read(SushiBindingModel model)
+        {
+            if (model == null)
+            {
+                return _productStorage.GetFullList();
+            }
+            if (model.Id.HasValue)
+            {
+                return new List<SushiViewModel> { _productStorage.GetElement(model)
+};
+            }
+            return _productStorage.GetFilteredList(model);
+        }
+        public void CreateOrUpdate(SushiBindingModel model)
+        {
+            var element = _productStorage.GetElement(new SushiBindingModel
+            {
+                SushiName = model.SushiName
+            });
+            if (element != null && element.Id != model.Id)
+            {
+                throw new Exception("Уже есть компонент с таким названием");
+            }
+            if (model.Id.HasValue)
+            {
+                _productStorage.Update(model);
+            }
+            else
+            {
+                _productStorage.Insert(model);
+            }
+        }
+        public void Delete(SushiBindingModel model)
+        {
+            var element = _productStorage.GetElement(new SushiBindingModel
+            {
+                Id =
+           model.Id
+            });
+            if (element == null)
+            {
+                throw new Exception("Элемент не найден");
+            }
+            _productStorage.Delete(model);
+        }
+    }
+}
---
+++ SushiBarView/FormSushi.cs
@@ -0,0 +1,176 @@
+﻿using SushiBarBusinessLogic.BindingModels;
+using SushiBarBusinessLogic.BusinessLogics;
+using SushiBarBusinessLogic.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Unity;
+namespace SushiBarView
+{
+    public partial class FormSushi : Form
+    {
+        [Dependency]
+        public new IUnityContainer Container { get; set; }
+        public int Id { set { id = value; } }
+        private readonly SushiLogic logic;
+        private int? id;
+        private Dictionary<int, (string, int)> sushiComponents;
+        public FormSushi(SushiLogic service)
+        {
+            InitializeComponent();
+            this.logic = service;
+        }
+        private void FormProduct_Load(object sender, EventArgs e)
+        {
+            if (id.HasValue)
+            {
+                try
+                {
+                    SushiViewModel view = logic.Read(new SushiBindingModel
+                    {
+                        Id =id.Value
+                    })?[0];
+                    if (view != null)
+                    {
+                        textBoxName.Text = view.SushiName;
+                        textBoxPrice.Text = view.Price.ToString();
+                        sushiComponents = view.SushiComponents;
+                        LoadData();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+                   MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                sushiComponents = new Dictionary<int, (string, int)>();
+            }
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                if (sushiComponents != null)
+                {
+                    DataGridView.Rows.Clear();
+                    foreach (var pc in sushiComponents)
+                    {
+                        DataGridView.Rows.Add(new object[] { pc.Key, pc.Value.Item1,
+pc.Value.Item2 });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+            }
+        }
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<FormSushiIngredient>();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                if (sushiComponents.ContainsKey(form.Id))
+                {
+                    sushiComponents[form.Id] = (form.IngredientName, form.Count);
+                }
+                else
+                {
+                    sushiComponents.Add(form.Id, (form.IngredientName, form.Count));
+                }
+                LoadData();
+            }
+        }
+        private void ButtonUpd_Click(object sender, EventArgs e)
+        {
+            if (DataGridView.SelectedRows.Count == 1)
+            {
+                var form = Container.Resolve<FormSushiIngredient>();
+                int id = Convert.ToInt32(DataGridView.SelectedRows[0].Cells[0].Value);
+                form.Id = id;
+                form.Count = sushiComponents[id].Item2;
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    sushiComponents[form.Id] = (form.IngredientName, form.Count);
+                    LoadData();
+                }
+            }
+        }
+        private void ButtonDel_Click(object sender, EventArgs e)
+        {
+            if (DataGridView.SelectedRows.Count == 1)
+            {
+                if (MessageBox.Show("Удалить запись", "Вопрос", MessageBoxButtons.YesNo,
+               MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    try
+                    {
+
+                        sushiComponents.Remove(Convert.ToInt32(DataGridView.SelectedRows[0].Cells[0].Value));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+                       MessageBoxIcon.Error);
+                    }
+                    LoadData();
+                }
+            }
+        }
+        private void ButtonRef_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxName.Text))
+            {
+                MessageBox.Show("Заполните название", "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(textBoxPrice.Text))
+            {
+                MessageBox.Show("Заполните цену", "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+                return;
+            }
+            if (sushiComponents == null || sushiComponents.Count == 0)
+            {
+                MessageBox.Show("Заполните ингредиенты", "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                logic.CreateOrUpdate(new SushiBindingModel
+                {
+                    Id = id,
+                    SushiName = textBoxName.Text,
+                    Price = Convert.ToDecimal(textBoxPrice.Text),
+                    SushiIngredients = sushiComponents
+                });
+                MessageBox.Show("Сохранение прошло успешно", "Сообщение",
+               MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+            }
+        }
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+    }
+}
