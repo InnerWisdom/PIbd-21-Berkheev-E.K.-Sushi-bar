17eea229fd824819fe61d2256dcfa5bb3a0251c1
Emil InnerW <emil4uk273@gmail.com>
2021-03-16 01:30:00 +0400

    first

---
+++ SushiBarDatabaseImplement/Migrations/20210315211400_InitialCreateSample.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SushiBarDatabaseImplement.Migrations
+{
+    public partial class InitialCreateSample : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
